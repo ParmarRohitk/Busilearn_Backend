b20398f562a75ba81046e76569f0ed38fe69db59
ParmarRohitk <parmarrohitk.bca@gmail.com>
2025-11-26 00:18:53 +0530

    feat: add MongoDB connection setup, update package dependencies, and refine environment configuration.

--- db_connect/database.js
+++ db_connect/database.js
@@ -1,19 +1,16 @@
 const mongoose = require("mongoose");
 
-module.exports = () =>{
+module.exports = () => {
 
     mongoose.set('strictQuery', true);
-    mongoose.connect(process.env.MONGODB_URI,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-.then((data)=>{
-    console.log(`Mongodb connected with server: ${data.connection.host}`);
-})
-.catch((err)=>{
-    console.log(`Error: ${err.message}`);
-    process.exit(2);    
-})
+    mongoose.connect(process.env.MONGODB_URI)
+        .then((data) => {
+            console.log(`Mongodb connected with server: ${data.connection.host}`);
+        })
+        .catch((err) => {
+            console.log(`Error: ${err.message}`);
+            process.exit(2);
+        })
 }
 
 
